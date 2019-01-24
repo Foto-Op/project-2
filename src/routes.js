15240481b93c366f23262ecebb10de2c1052a0d4
Patric Khiev <webdev.patric@gmail.com>
2019-01-24 13:59:27 -0800

    fixed routes setup

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
+// Pages
 import Gallery from "./components/gallery/Gallery";
 import Landing from "./components/landing/Landing";
 import Navbar from "./components/navbar/Navbar";
 
 export default (
   <Switch>
+    <Route exact path="/" component={Landing} />
     <Route path="/gallery" component={Gallery} />
-    <Route path="/landing" component={Landing} />
     <Route path="/navbar" component={Navbar} />
+    <Route path="*" render={() => <img src="https://cdn-images-1.medium.com/max/1600/1*qdFdhbR00beEaIKDI_WDCw.gif" alt ='whooopies' />} />
+
+
   </Switch>
 );
