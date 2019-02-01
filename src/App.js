8b6454dee233b0bb3827e1d152d9efd9c25f40ec
Patric Khiev <webdev.patric@gmail.com>
2019-01-31 18:56:48 -0800

    minor landing page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import './App.scss';
 import { BrowserRouter } from 'react-router-dom';
 import routes from './routes';
-import NavBar from './components/navbar/Navbar';
+
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className='App'>
-          <h1>Wasssup from Dan</h1>
-          <h1>Hi from Patric</h1>
-          <NavBar />
           {routes}
         </div>
       </BrowserRouter>
