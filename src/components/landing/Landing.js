46dd6d83fbdec9d16ae61ec47ad7424c3acf3aa5
Patric Khiev <webdev.patric@gmail.com>
2019-02-13 15:34:20 -0800

    added background

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,14 +1,22 @@
-import React, { Component } from "react";
-import "./Landing.scss";
+import React, { Component } from 'react';
+import './Landing.scss';
 
 class Landing extends Component {
   render() {
     return (
-      <div className="landing">
-        <div className='landing landing__top'>Top</div>
-        <div className='landing landing__navbar'>Navbar</div>
+      <div className='landing'>
+        <div className='landing landing__top'>
+          <h1>DERECK ZOOLANDER</h1>
+        </div>
+        <div className='landing landing__navbar'>
+          <h4>About Me</h4>
+          <h4>Contact</h4>
+          <h4>Appointments</h4>
+        </div>
         <div className='landing landing__body'>Drone Gif</div>
-        <div className='landing landing__lowerbody'>Click Me/Scroll Animation</div>
+        <div className='landing landing__lowerbody'>
+          Click Me/Scroll Animation
+        </div>
       </div>
     );
   }
